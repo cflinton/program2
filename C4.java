7b3376ae6f3ad5b6d21e25e95b3640017812777f
Carl Ferguson Linton <cflinton@prdile.(none)>
2016-02-14 16:04:57 -0500

    finalized version

--- C4.java
+++ C4.java
@@ -3,7 +3,9 @@
  * Program 2 - MyGame
  * CSC230-02 Spring 2016
  */
-package mygame;
+
+
+import java.util.Arrays;
 
 public class C4 extends Game {
 
@@ -19,17 +21,14 @@ public C4(){
 		}
 	}
 
-public static void clearBoard(){
+public void clearBoard(){
 	for (int[] row: board){ //loops through each row in board
-		for (int i: row){ //loops through each int in each row
-			i = 0; //sets the int to zero
-			}
-		}
+		Arrays.fill(row, 0);
 	}
-
-public static int getPiece(int row, int col){
+	}
+public int getPiece(int row, int col){
 	int i = 3;
-	if((row <= ROWS) && (col <= COLS) && (row > 0) && (col > 0)){ //checks to see if paramaters are in bounds (assumes that the first row and column are numbered 1 instead of 0)
+	if((0 < row) && (row <= ROWS) && (col <= COLS) && (col > 0)){ //checks to see if paramaters are in bounds (assumes that the first row and column are numbered 1 instead of 0)
 		i = board[row - 1][col - 1];
 		} else{
 		System.out.println("Value out of bounds, returning three.");
@@ -37,12 +36,12 @@ public static int getPiece(int row, int col){
 	return i;
 	}
 
-public static int getTurn(){
+public int getTurn(){
 	int i = c4Game.currentTurn();
 	return i;
 	}
 
-public static boolean isColFull(int col){
+public boolean isColFull(int col){
 	boolean isFull = false; //says wheather or not the column is full
 	int row = 1; //keeps track of row number
 	int num = 3; //holds the number in the last checked space in the column
@@ -60,7 +59,7 @@ public static boolean isColFull(int col){
 	return isFull;
 	}
 
-public static boolean isDiagWinner(){
+public boolean isDiagWinner(){
 	boolean isWin = false; //determines if there is a win
 	int r = 1; //row counter
 	int c = 1; //column counter
@@ -71,36 +70,36 @@ public static boolean isDiagWinner(){
 			n = 0;
 			p = this.getPiece(r, c);
 			if (p != 0) { //if a player has placed a piece in the checked spot, the search for a win begins
-				n = 1; //if a player has placed a piece in the checked spot then it is the first in a line of unknown length
 				tc = c;
 				tr = r;
 				tp = p;
 				while((tp == p) && (tr > 0) && (tc <= COLS) && (!isWin)) { //loop stops if win is found, non matching spot is found, or the search tries to go out of bounds
-					tc++; // moves column for searching to the right
-					tr--; // moves row for searching down
 					tp = this.getPiece(tr, tc); //gets the value of the searched piece
 					if(tp == p){ //if the piece was placed by the same player then the length of the line is increased by one
 						n++;
-						}
+					}
 					if(n == 4){ //if the line is four long then a win has been found
 						isWin = true;
-						}
 					}
-				n = 1; //resets values to search for a diagonal win a different way
+					tc++; // moves column for searching to the right
+					tr--; // moves row for searching down
+				}
+				n = 0; //resets values to search for a diagonal win a different way
 				tc = c;
 				tr = r;
 				tp = p;
-				while((tp == p) && (tr <= ROWS) && (tc <= COLS) && (!isWin) { //loop stops for win, non-match, or out of bounds
-					tc++; //moves search right
-					tr++; //moves search up
+				while((tp == p) && (tr <= ROWS) && (tc <= COLS) && (!isWin)) { //loop stops for win, non-match, or out of bounds
 					tp = this.getPiece(tr, tc);
 					if(tp == p){ //length is one longer if match
 						n++;
-						}
+					}
 					if(n == 4){ //four long means a win
 						isWin = true;
-						}
-					}
+				}
+					tc++; //moves search right
+					tr++; //moves search up
+
+				}
 				}
 			c++;
 			}
@@ -110,7 +109,7 @@ public static boolean isDiagWinner(){
 	return isWin;
 	}
 
-public static boolean isFull(){
+public boolean isFull(){
 	boolean full = true;
 	int r = 1;
 	int c = 1;
@@ -129,42 +128,39 @@ public static boolean isFull(){
 	return full;
 	}
 
-public static boolean isHorizWinner(){
+public boolean isHorizWinner(){
 	boolean isWin = false; //determines if there is a win
 	int r = 1; //row counter
 	int c = 1; //column counter
-	int n = 0; //number of matches in a diagonal line
+	int n = 0; //number of matches in a horizontal line
 	int tr, tc, p, tp; //temp row counter, temp column counter, what number is in certain place, and teporary marker of what is in a certain place
 	while((r <= ROWS) && (!isWin)){ //loop runs through each row, stops if win is found
 		while((c <= COLS) && (!isWin)) { //loop runs through each column for each row, stops if win is found
 			n = 0;
 			p = this.getPiece(r, c);
 			if (p != 0) { //if a player has placed a piece in the checked spot, the search for a win begins
-				n = 1; //if a player has placed a piece in the checked spot then it is the first in a line of unknown length
 				tc = c;
 				tp = p;
 				while((tp == p) && (r > 0) && (tc <= COLS) && (!isWin)) { //loop stops if win is found, non matching spot is found, or the search tries to go out of bounds
-					tc++; // moves column for searching to the right
 					tp = this.getPiece(r, tc); //gets the value of the searched piece
 					if(tp == p){ //if the piece was placed by the same player then the length of the line is increased by one
 						n++;
-						}
+					}
 					if(n == 4){ //if the line is four long then a win has been found
 						isWin = true;
-						}
 					}
+					tc++; // moves column for searching to the right
 				}
-			c++;
 			}
+			c++;
+		}
 		c = 1; //resets column value
 		r++;
-		}
+	}
 	return isWin;
 	}
 
-	}
-
-public static boolean isVertWinner(){
+public boolean isVertWinner(){
 	boolean isWin = false; //determines if there is a win
 	int r = 1; //row counter
 	int c = 1; //column counter
@@ -175,72 +171,81 @@ public static boolean isVertWinner(){
 			n = 0;
 			p = this.getPiece(r, c);
 			if (p != 0) { //if a player has placed a piece in the checked spot, the search for a win begins
-				n = 1; //if a player has placed a piece in the checked spot then it is the first in a line of unknown length
 				tr = r;
 				tp = p;
 				while((tp == p) && (tr > 0) && (c <= COLS) && (!isWin)) { //loop stops if win is found, non matching spot is found, or the search tries to go out of bounds
-					tr--; // moves ro for searching down
 					tp = this.getPiece(tr, c); //gets the value of the searched piece
 					if(tp == p){ //if the piece was placed by the same player then the length of the line is increased by one
 						n++;
-						}
+					}
 					if(n == 4){ //if the line is four long then a win has been found
 						isWin = true;
-						}
 					}
+					tr--; // moves row for searching down
 				}
-			c++;
 			}
+			c++;
+		}
 		c = 1; //resets column value
 		r++;
-		}
+	}
 	return isWin;
 	}
 
+public boolean isWinner(){
+	boolean win = false;
+
+	if(this.isDiagWinner() || this.isHorizWinner() || this.isVertWinner()) {
+		win = true;
 	}
 
-public static void nextTurn(){
+	return win;
+	}
+
+public void nextTurn(){
 	c4Game.next();
 	}
 
-public static boolean playPiece(int col){ //outputs whether or not the piece has been placed
+public boolean playPiece(int col){ //outputs whether or not the piece has been placed
 	boolean placed = false; //says whether or not the piece was placed
 	int r = ROWS; //counter keeps track of rows
 	int p; //place holder for what piece is being checked
 	if((col > COLS) || (col <= 0)){ //checks to see if col is out of bounds
-		System.out.print("Column number out of range, insert new number: "); //says why the piece couldn't be placed, defers to MyGame logic for new input
-		} else if(0 != getPiece(1, col)){ //checks to see if the top row of that column has a piece, and therefore is full
-			System.out.print("Column is full, select a different Column");
-			} else while((r > 0) && (!placed)){
-				p = getPiece(r, col);
-				if(p == 0){
-					board[r - 1][col - 1] = c4game.currentTurn();
-					placed = true;
-					}
-				r--;
-				}
+		System.out.println("Column number out of range, insert new number: "); //says why the piece couldn't be placed, defers to MyGame logic for new input
+	} else if(0 != getPiece(1, col)){ //checks to see if the top row of that column has a piece, and therefore is full
+		System.out.println("Column is full, select a different Column");
+	} else while((r > 0) && (!placed)){
+		p = getPiece(r, col);
+		if(p == 0){
+			board[r - 1][col - 1] = c4Game.currentTurn();
+			placed = true;
+		}
+		r--;
+	}
 	return placed;
 	}
 
-public static void printBoard(){
+public void printBoard(){
 	int r = 1;
 	int c = 1;
-
-	System.out.print("     ");
+	int p;
+	System.out.print("      ");
 
 	while(c <= COLS){
 		System.out.print("" + c + " ");
-		}
+		c++;
+	}
 
 	System.out.print("\n");
 
 	c = 1;
 	
-	System.out.print("     ");
+	System.out.print("      ");
 
 	while(c <= COLS){
 		System.out.print("--");
-		}
+		c++;
+	}
 
 	System.out.print("\n");
 
@@ -248,22 +253,23 @@ public static void printBoard(){
 	c = 1;
 	
 	while(r <= ROWS){
-		System.out.print("" + r + "   " + "|  ");
+		System.out.print("" + r + "  " + "|  ");
 		while(c <= COLS){
 			p = getPiece(r, c);
 			System.out.print("" + p + " ");
 			c++;
 			}
-		System.out.print("   |" + "\n");
+		System.out.print("  |" + "\n");
 		c = 1;
 		r++;
 		}
 
-	System.out.print("     ");
+	System.out.print("      ");
 
 	while(c <= COLS){
 		System.out.print("--");
-		}
+		c++;
+	}
 
 	System.out.print("\n");
 	}
--- MyGame.java
+++ MyGame.java
@@ -1,9 +1,9 @@
 /*
- * Author: 
+ * Author: Carl Linton
  * Program 2 - MyGame
  * CSC230-02 Spring 2016
  */
-package mygame;
+import java.util.Scanner;
 
 public class MyGame {
 
@@ -11,7 +11,58 @@ public class MyGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        C4 mg = new C4();
+	boolean win = false;
+	boolean full = false;
+	boolean place;
+	Scanner scan = new Scanner(System.in);
+	int c;
+	String q;
+	
+	scan.next();	
+	
+	System.out.println("Hello there!");
+	
+	while((!win) && (!full)){
+		mg.printBoard();
+		q = "n";
+		place = false;
+		
+		System.out.println("Welcome to Connect 4!");
+		
+		while(!place) {
+			System.out.println("Player " + mg.getTurn() + " pick a column to place your piece: ");
+			c = scan.nextInt();
+			place = mg.playPiece(c);
+		}
+		
+		full = mg.isFull();
+		
+		win = mg.isWinner();
+
+		if(win) {
+			mg.c4Game.winner();
+			System.out.println("Would you like to play again? Y/N: ");
+			q = scan.next();
+		} else if(full) {
+			System.out.println("The board is full and there are no matches, the game is a draw.");
+			System.out.print("Would you like to play again? Y/N: ");
+			q = scan.next();
+		}
+		
+		mg.nextTurn();
+		
+		q = q.toUpperCase();
+		
+		if(q.equals("Y")){
+			System.out.println("Reseting Connect Four");
+			mg.clearBoard();
+			win = false;
+			full = false;
+			mg.c4Game.reset();
+			
+		}
     }
-    
+		System.out.println("Goodbye!");
+    }
 }
